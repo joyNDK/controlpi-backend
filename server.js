0f804a66d7d5ef664d2530f759bcbd7a5ad5ab4d
Nathan-Joy KAYEMBE NDALAMBA <nathanjoyn@gmail.com>
2025-11-28 20:12:57 +0200

    Fix package.json pour Render

--- server.js
+++ server.js
@@ -3,12 +3,10 @@ const path = require("path");
 
 const app = express();
 
-// Route explicite pour servir la clé Pi
 app.get("/validation-key.txt", (req, res) => {
   res.sendFile(path.join(__dirname, "validation-key.txt"));
 });
 
-// Route de test
 app.get("/", (req, res) => {
   res.send("✅ ControlPi Backend en ligne !");
 });
